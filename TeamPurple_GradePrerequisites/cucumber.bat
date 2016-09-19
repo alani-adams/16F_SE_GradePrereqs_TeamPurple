d3835a03cb02dfb31be882325791e7aa247c555a
Sebmansigh <sjs14b@acu.edu>
2016-09-19 14:27:21 -0500

    Add files via upload

--- TeamPurple_GradePrerequisites/cucumber.bat
+++ TeamPurple_GradePrerequisites/cucumber.bat
@@ -1,1 +1,2 @@
-stuff
+javac -cp "jars/*;." step_definitions/GradePrerequisitesTest.java implementation/GradePrerequisites.java
+java -cp "jars/*;." cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features
