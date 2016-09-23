538345606952b5d2d2e1b8323c5958d0b3bf4b75
alp13d <alp13d@acu.edu>
2016-09-22 22:29:03 -0500

    Update GradePrerequisites.java

--- TeamPurple_GradePrerequisites/implementation/GradePrerequisites.java
+++ TeamPurple_GradePrerequisites/implementation/GradePrerequisites.java
@@ -1,12 +1,4 @@
 //
-//  THIS IS A SKELETON. ON EACH NEW ECLIPSE PROJECT:
-//    Edit the batch file to reflect the actual test class and base class files.
-//    Edit the comment below to match the batch file.
-//    Properly set Cucumber's run configuration.
-//    Remove this checklist comment.
-//    
-
-//
 //  Compile with:
 //      javac -cp "jars/*;." step_definitions/BaseTest.java implementation/BaseClass.java
 //      java -cp "jars/*;." cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features
