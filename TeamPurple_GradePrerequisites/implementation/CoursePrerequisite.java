536471eb9a271427f1116ca8ca004d55742e4fde
alp13d <alp13d@acu.edu>
2016-09-22 22:40:42 -0500

    Create CoursePrerequisite.java

---
+++ TeamPurple_GradePrerequisites/implementation/CoursePrerequisite.java
@@ -0,0 +1,18 @@
+
+public class CoursePrerequisite{
+  private String courseCode;
+  private char minGrade;
+  
+  public CoursePrerequisite( String code, char grade){
+    courseCode = code;
+    minGrade = grade;
+  }
+  
+  public String getCourseCode(){
+    return courseCode;
+  }
+  
+  public char getMinGrade(){
+    return minGrade;
+  }
+}
