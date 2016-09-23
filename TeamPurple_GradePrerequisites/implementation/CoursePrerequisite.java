960c7c9b66041140055eac1110376fb461edc558
alp13d <alp13d@acu.edu>
2016-09-22 23:30:39 -0500

    Add files via upload

--- TeamPurple_GradePrerequisites/implementation/CoursePrerequisite.java
+++ TeamPurple_GradePrerequisites/implementation/CoursePrerequisite.java
@@ -1,24 +1,34 @@
 package implementation;
 
 public class CoursePrerequisite{
-  private String courseCode;
-  private char minGrade;
+    private String courseCode;
+    private char minGrade;
   
-  public CoursePrerequisite( String code, char grade){
+    public CoursePrerequisite( String code, char grade)
+    {
     courseCode = code;
     minGrade = grade;
-  }
+    }
   
-  public String getCourseCode(){
-    return courseCode;
-  }
+    public String GetCourseCode()
+    {
+        return courseCode;
+    }
   
-  public char getMinGrade(){
-    return minGrade;
-  }
+    public char GetMinGrade(){
+        return minGrade;
+    }
   
-  @Override
-  public boolean isMetBy(Student stu){
-    return true;
+    @Override
+    public boolean IsMetBy(Student stu){
+        ArrayList<Course> cour = stu.GetTakenCourses();
+        for(Course c : cour)
+        {
+            if(courseCode.equals(c.GetCourseCode))
+                if((char)(c.GetGrade()) >= GetMinGrade())
+                    return true;
+        }
+      
+    return false;
   }
 }
