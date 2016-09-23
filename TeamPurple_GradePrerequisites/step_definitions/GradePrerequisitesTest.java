ab9efbfd100f6b9188753d9b0baf0440e7e76b26
Sebmansigh <sjs14b@acu.edu>
2016-09-22 21:01:10 -0500

    Add files via upload

--- TeamPurple_GradePrerequisites/step_definitions/GradePrerequisitesTest.java
+++ TeamPurple_GradePrerequisites/step_definitions/GradePrerequisitesTest.java
@@ -6,29 +6,57 @@ import cucumber.api.java.en.*;
 import cucumber.api.java.Before;
 
 import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
 /**
  * @author Sebastian Snyder
  */
 public class GradePrerequisitesTest
 {
+	HashMap<String,Student> Students;
+	Course C;
+	
+	Student TestStudent;
+	
 	@Before
 	public void Before()
 	{
-		GradePrerequisites G = new GradePrerequisites();
-		G.equals(G);
+		Students = new HashMap<String,Student>();
 	}
 	
-    @Given("^\"(.*?)\" and \"(.*?)\"$")
-    public void and(String banner, String crn) throws Throwable {
-        
-        
-    }
-    
-    @Then("^the student is \"(.*?)\" to be enrolled in the course$")
-    public void theStudentIsToBeEnrolledInTheCourse(String enrollment) throws Throwable {
-        
-        
-    }
+	@Given("^\"(.*?)\" with a CRN of \"(.*?)\" with a prereq of \"(.*?)\"$")
+	public void with_a_CRN_of_with_a_prereq_of(String arg1, String arg2, String arg3) throws Throwable {
+	    Course.RegisterCourse(arg1,arg2,arg3);
+	}
+
+	@Given("^\"(.*?)\" has taken \"(.*?)\" and recieved a \"(.*?)\"$")
+	public void has_taken_and_recieved_a(String arg1, String arg2, String arg3) throws Throwable
+	{
+		Student S;
+		if(!Students.containsKey(arg1))
+		{
+			S = new Student(arg1);
+			Students.put(arg1, S);
+		}
+		else
+		{
+			S = Students.get(arg1);
+		}
+	    S.TakeCourse(Course.GetFromCRN(arg2,arg3.charAt(0)));
+	}
+
+	@Given("^A student with BannerID \"(.*?)\"$")
+	public void banner_and_CRN(String arg1) throws Throwable {
+	    TestStudent = Students.get(arg1);
+	}
+
+	@Then("^the student \"(.*?)\" allowed to be enrolled in \"(.*?)\"$")
+	public void the_student_allowed_to_be_enrolled_in_the_course(String arg1, String arg2) throws Throwable {
+	    boolean disallowed = arg1.equals("is not");
+	    Assert.assertTrue(disallowed ^ TestStudent.CanTakeCourse(Course.GetFromCRN(arg2)));
+	}
 
     
 }
