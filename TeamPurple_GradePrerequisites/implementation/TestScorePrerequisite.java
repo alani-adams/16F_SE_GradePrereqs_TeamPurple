afaca8146c6a0631a0869b80e4fc666970b925ba
cxs13b <cxs13b@acu.edu>
2016-09-26 21:06:17 -0500

    Add files via upload

---
+++ TeamPurple_GradePrerequisites/implementation/TestScorePrerequisite.java
@@ -0,0 +1,24 @@
+package implementation;
+
+class TestScorePrerequsite extends Prerequisite 
+{
+	String section;
+	int minimumScore;
+
+	public ACTScorePrerequsite(String subject, int min)
+	{
+		section = subject;
+		minimumScore = min;
+	}
+
+	@Override
+    public boolean IsMetBy(Student stu)
+    {
+    
+   		if (stu.getTestScore(section) >= minimumScore)
+   			return true;
+
+   		return false;
+    }
+
+}
